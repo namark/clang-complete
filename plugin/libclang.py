11c6cdda29279184cb95187008329d83584b6bc7
Steven Myint <git@stevenmyint.com>
2015-03-02 17:42:55 -0800

    Decode byte string

--- plugin/libclang.py
+++ plugin/libclang.py
@@ -530,7 +530,7 @@ def getCurrentCompletions(base):
   timer.registerEvent("Count # Results (%s)" % str(len(results)))
 
   if base != "":
-    results = [x for x in results if getAbbr(x.string).startswith(base.encode('utf-8'))]
+    results = [x for x in results if getAbbr(x.string).startswith(base)]
 
   timer.registerEvent("Filter")
 
@@ -553,7 +553,7 @@ def getCurrentCompletions(base):
 def getAbbr(strings):
   for chunks in strings:
     if chunks.isKindTypedText():
-      return chunks.spelling
+      return decode(chunks.spelling)
   return ""
 
 def jumpToLocation(filename, line, column, preview):
